2f18650cacc3043c6992cd9fca552e16af1e6d76
truongmta190298 <camamidol@gmail.com>
2019-11-18 23:00:03 +0700

    ductruong frmThemPN

--- QuanLyKhoHang/QuanLyKhoHang/frmThemPN.Designer.cs
+++ QuanLyKhoHang/QuanLyKhoHang/frmThemPN.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cboTenNCC = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cboTenNCC
+            // 
+            this.cboTenNCC.FormattingEnabled = true;
+            this.cboTenNCC.Location = new System.Drawing.Point(122, 2);
+            this.cboTenNCC.Margin = new System.Windows.Forms.Padding(4);
+            this.cboTenNCC.Name = "cboTenNCC";
+            this.cboTenNCC.Size = new System.Drawing.Size(276, 24);
+            this.cboTenNCC.TabIndex = 108;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.AccessibleDescription = "";
+            this.dateTimePicker1.AccessibleName = "";
+            this.dateTimePicker1.CustomFormat = "yyyy/MM/dd";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(535, 2);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(180, 22);
+            this.dateTimePicker1.TabIndex = 107;
+            this.dateTimePicker1.Value = new System.DateTime(2015, 4, 1, 0, 0, 0, 0);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(425, 12);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.TabIndex = 106;
+            this.label3.Text = "Ngày Nhập";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 105;
+            this.label2.Text = "Tên NCC";
+            // 
+            // frmThemPN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cboTenNCC);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "frmThemPN";
             this.Text = "frmThemPN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cboTenNCC;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
