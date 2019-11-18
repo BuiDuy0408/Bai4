c2f59bfe2c09661ff5f53a3928c99bd905cfa5a0
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-11-18 15:48:51 +0700

    thanh-add form nha cung cap

---
+++ QuanLyKhoHang/QuanLyKhoHang/frmNhaCungCap.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace QuanLyKhoHang
+{
+    partial class frmNhaCungCap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label9 = new System.Windows.Forms.Label();
+            this.cboTK = new System.Windows.Forms.ComboBox();
+            this.txtTK = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(41, 35);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(73, 13);
+            this.label9.TabIndex = 75;
+            this.label9.Text = "Tìm kiếm theo";
+            // 
+            // cboTK
+            // 
+            this.cboTK.FormattingEnabled = true;
+            this.cboTK.Items.AddRange(new object[] {
+            "Mã NCC",
+            "Tên NCC",
+            "Địa Chỉ",
+            "SĐT"});
+            this.cboTK.Location = new System.Drawing.Point(120, 32);
+            this.cboTK.Name = "cboTK";
+            this.cboTK.Size = new System.Drawing.Size(161, 21);
+            this.cboTK.TabIndex = 76;
+            // 
+            // txtTK
+            // 
+            this.txtTK.Location = new System.Drawing.Point(358, 35);
+            this.txtTK.Name = "txtTK";
+            this.txtTK.Size = new System.Drawing.Size(264, 20);
+            this.txtTK.TabIndex = 77;
+            // 
+            // frmNhaCungCap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 279);
+            this.Controls.Add(this.txtTK);
+            this.Controls.Add(this.cboTK);
+            this.Controls.Add(this.label9);
+            this.Name = "frmNhaCungCap";
+            this.Text = "frmNhaCungCap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cboTK;
+        private System.Windows.Forms.TextBox txtTK;
+    }
+}
