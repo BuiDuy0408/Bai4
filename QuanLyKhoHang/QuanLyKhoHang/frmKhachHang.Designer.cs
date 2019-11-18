257ba5a03de1856eb3dbb8ac27429cbb653681e2
BuiDuy0408 <54673782+BuiDuy0408@users.noreply.github.com>
2019-11-18 23:00:12 +0700

    Duy - add khach hang

--- QuanLyKhoHang/QuanLyKhoHang/frmKhachHang.Designer.cs
+++ QuanLyKhoHang/QuanLyKhoHang/frmKhachHang.Designer.cs
@@ -28,11 +28,322 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmKhachHang));
+            this.panelKH = new System.Windows.Forms.Panel();
+            this.lblTopic = new System.Windows.Forms.Label();
+            this.pictureBoxKH = new System.Windows.Forms.PictureBox();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.cmbGioiTinh = new System.Windows.Forms.ComboBox();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.lblGioiTinh = new System.Windows.Forms.Label();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.lblGhichu = new System.Windows.Forms.Label();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.txtGhichu = new System.Windows.Forms.TextBox();
+            this.cmbLoaiKH = new System.Windows.Forms.ComboBox();
+            this.lblLoaiKH = new System.Windows.Forms.Label();
+            this.txtSoDT = new System.Windows.Forms.TextBox();
+            this.txtDC = new System.Windows.Forms.TextBox();
+            this.txttenKH = new System.Windows.Forms.TextBox();
+            this.txtMaKH = new System.Windows.Forms.TextBox();
+            this.lblSoDT = new System.Windows.Forms.Label();
+            this.lblDiaCHi = new System.Windows.Forms.Label();
+            this.lblTenKH = new System.Windows.Forms.Label();
+            this.lblMaKH = new System.Windows.Forms.Label();
+            this.panelKH.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxKH)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelKH
+            // 
+            this.panelKH.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panelKH.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelKH.Controls.Add(this.lblTopic);
+            this.panelKH.Controls.Add(this.pictureBoxKH);
+            this.panelKH.Controls.Add(this.btnXoa);
+            this.panelKH.Controls.Add(this.cmbGioiTinh);
+            this.panelKH.Controls.Add(this.btnSua);
+            this.panelKH.Controls.Add(this.lblGioiTinh);
+            this.panelKH.Controls.Add(this.btnLuu);
+            this.panelKH.Controls.Add(this.lblGhichu);
+            this.panelKH.Controls.Add(this.btnThem);
+            this.panelKH.Controls.Add(this.txtGhichu);
+            this.panelKH.Controls.Add(this.cmbLoaiKH);
+            this.panelKH.Controls.Add(this.lblLoaiKH);
+            this.panelKH.Controls.Add(this.txtSoDT);
+            this.panelKH.Controls.Add(this.txtDC);
+            this.panelKH.Controls.Add(this.txttenKH);
+            this.panelKH.Controls.Add(this.txtMaKH);
+            this.panelKH.Controls.Add(this.lblSoDT);
+            this.panelKH.Controls.Add(this.lblDiaCHi);
+            this.panelKH.Controls.Add(this.lblTenKH);
+            this.panelKH.Controls.Add(this.lblMaKH);
+            this.panelKH.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelKH.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelKH.Location = new System.Drawing.Point(0, 0);
+            this.panelKH.Name = "panelKH";
+            this.panelKH.Size = new System.Drawing.Size(1069, 222);
+            this.panelKH.TabIndex = 5;
+            // 
+            // lblTopic
+            // 
+            this.lblTopic.AutoSize = true;
+            this.lblTopic.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTopic.ForeColor = System.Drawing.Color.Red;
+            this.lblTopic.Location = new System.Drawing.Point(342, 15);
+            this.lblTopic.Name = "lblTopic";
+            this.lblTopic.Size = new System.Drawing.Size(238, 27);
+            this.lblTopic.TabIndex = 23;
+            this.lblTopic.Text = "Thông Tin Khách Hàng";
+            // 
+            // pictureBoxKH
+            // 
+            this.pictureBoxKH.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.pictureBoxKH.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxKH.BackgroundImage")));
+            this.pictureBoxKH.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBoxKH.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBoxKH.Location = new System.Drawing.Point(821, 5);
+            this.pictureBoxKH.Name = "pictureBoxKH";
+            this.pictureBoxKH.Size = new System.Drawing.Size(237, 210);
+            this.pictureBoxKH.TabIndex = 22;
+            this.pictureBoxKH.TabStop = false;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnXoa.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnXoa.BackgroundImage")));
+            this.btnXoa.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnXoa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnXoa.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.ForeColor = System.Drawing.Color.DarkGray;
+            this.btnXoa.Location = new System.Drawing.Point(11, 125);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(128, 39);
+            this.btnXoa.TabIndex = 8;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = false;
+            // 
+            // cmbGioiTinh
+            // 
+            this.cmbGioiTinh.FormattingEnabled = true;
+            this.cmbGioiTinh.Items.AddRange(new object[] {
+            "Nam",
+            "Nữ",
+            "Gay",
+            "Less"});
+            this.cmbGioiTinh.Location = new System.Drawing.Point(306, 137);
+            this.cmbGioiTinh.Name = "cmbGioiTinh";
+            this.cmbGioiTinh.Size = new System.Drawing.Size(121, 21);
+            this.cmbGioiTinh.TabIndex = 21;
+            // 
+            // btnSua
+            // 
+            this.btnSua.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSua.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSua.BackgroundImage")));
+            this.btnSua.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSua.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSua.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSua.ForeColor = System.Drawing.Color.DarkGray;
+            this.btnSua.Location = new System.Drawing.Point(11, 73);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(128, 37);
+            this.btnSua.TabIndex = 7;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = false;
+            // 
+            // lblGioiTinh
+            // 
+            this.lblGioiTinh.AutoSize = true;
+            this.lblGioiTinh.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGioiTinh.Location = new System.Drawing.Point(178, 145);
+            this.lblGioiTinh.Name = "lblGioiTinh";
+            this.lblGioiTinh.Size = new System.Drawing.Size(72, 19);
+            this.lblGioiTinh.TabIndex = 20;
+            this.lblGioiTinh.Text = "Giới Tính";
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLuu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnLuu.BackgroundImage")));
+            this.btnLuu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLuu.Enabled = false;
+            this.btnLuu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLuu.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLuu.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLuu.Location = new System.Drawing.Point(11, 177);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(128, 38);
+            this.btnLuu.TabIndex = 6;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = false;
+            // 
+            // lblGhichu
+            // 
+            this.lblGhichu.AutoSize = true;
+            this.lblGhichu.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGhichu.Location = new System.Drawing.Point(542, 145);
+            this.lblGhichu.Name = "lblGhichu";
+            this.lblGhichu.Size = new System.Drawing.Size(60, 19);
+            this.lblGhichu.TabIndex = 19;
+            this.lblGhichu.Text = "Ghi chú";
+            // 
+            // btnThem
+            // 
+            this.btnThem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnThem.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnThem.BackgroundImage")));
+            this.btnThem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnThem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnThem.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnThem.Location = new System.Drawing.Point(11, 15);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(128, 41);
+            this.btnThem.TabIndex = 5;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = false;
+            // 
+            // txtGhichu
+            // 
+            this.txtGhichu.Location = new System.Drawing.Point(652, 139);
+            this.txtGhichu.Multiline = true;
+            this.txtGhichu.Name = "txtGhichu";
+            this.txtGhichu.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txtGhichu.Size = new System.Drawing.Size(163, 60);
+            this.txtGhichu.TabIndex = 18;
+            // 
+            // cmbLoaiKH
+            // 
+            this.cmbLoaiKH.Enabled = false;
+            this.cmbLoaiKH.FormattingEnabled = true;
+            this.cmbLoaiKH.Items.AddRange(new object[] {
+            "Khách VIP",
+            "Khách Thường Xuyên",
+            "Khách Lẻ"});
+            this.cmbLoaiKH.Location = new System.Drawing.Point(306, 172);
+            this.cmbLoaiKH.Name = "cmbLoaiKH";
+            this.cmbLoaiKH.Size = new System.Drawing.Size(163, 21);
+            this.cmbLoaiKH.TabIndex = 17;
+            // 
+            // lblLoaiKH
+            // 
+            this.lblLoaiKH.AutoSize = true;
+            this.lblLoaiKH.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoaiKH.Location = new System.Drawing.Point(178, 175);
+            this.lblLoaiKH.Name = "lblLoaiKH";
+            this.lblLoaiKH.Size = new System.Drawing.Size(126, 19);
+            this.lblLoaiKH.TabIndex = 16;
+            this.lblLoaiKH.Text = "Loại Khách Hàng";
+            // 
+            // txtSoDT
+            // 
+            this.txtSoDT.Enabled = false;
+            this.txtSoDT.Location = new System.Drawing.Point(652, 66);
+            this.txtSoDT.Name = "txtSoDT";
+            this.txtSoDT.Size = new System.Drawing.Size(163, 20);
+            this.txtSoDT.TabIndex = 15;
+            // 
+            // txtDC
+            // 
+            this.txtDC.Enabled = false;
+            this.txtDC.Location = new System.Drawing.Point(652, 101);
+            this.txtDC.Multiline = true;
+            this.txtDC.Name = "txtDC";
+            this.txtDC.Size = new System.Drawing.Size(163, 25);
+            this.txtDC.TabIndex = 14;
+            // 
+            // txttenKH
+            // 
+            this.txttenKH.Enabled = false;
+            this.txttenKH.Location = new System.Drawing.Point(306, 101);
+            this.txttenKH.Name = "txttenKH";
+            this.txttenKH.Size = new System.Drawing.Size(209, 20);
+            this.txttenKH.TabIndex = 13;
+            // 
+            // txtMaKH
+            // 
+            this.txtMaKH.Enabled = false;
+            this.txtMaKH.Location = new System.Drawing.Point(306, 66);
+            this.txtMaKH.Name = "txtMaKH";
+            this.txtMaKH.Size = new System.Drawing.Size(209, 20);
+            this.txtMaKH.TabIndex = 12;
+            // 
+            // lblSoDT
+            // 
+            this.lblSoDT.AutoSize = true;
+            this.lblSoDT.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoDT.Location = new System.Drawing.Point(542, 69);
+            this.lblSoDT.Name = "lblSoDT";
+            this.lblSoDT.Size = new System.Drawing.Size(104, 19);
+            this.lblSoDT.TabIndex = 11;
+            this.lblSoDT.Text = "Số Điện Thoại";
+            // 
+            // lblDiaCHi
+            // 
+            this.lblDiaCHi.AutoSize = true;
+            this.lblDiaCHi.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiaCHi.Location = new System.Drawing.Point(542, 104);
+            this.lblDiaCHi.Name = "lblDiaCHi";
+            this.lblDiaCHi.Size = new System.Drawing.Size(60, 19);
+            this.lblDiaCHi.TabIndex = 10;
+            this.lblDiaCHi.Text = "Địa Chỉ";
+            // 
+            // lblTenKH
+            // 
+            this.lblTenKH.AutoSize = true;
+            this.lblTenKH.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTenKH.Location = new System.Drawing.Point(178, 104);
+            this.lblTenKH.Name = "lblTenKH";
+            this.lblTenKH.Size = new System.Drawing.Size(122, 19);
+            this.lblTenKH.TabIndex = 9;
+            this.lblTenKH.Text = "Tên Khách Hàng";
+            // 
+            // lblMaKH
+            // 
+            this.lblMaKH.AutoSize = true;
+            this.lblMaKH.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaKH.Location = new System.Drawing.Point(178, 69);
+            this.lblMaKH.Name = "lblMaKH";
+            this.lblMaKH.Size = new System.Drawing.Size(120, 19);
+            this.lblMaKH.TabIndex = 8;
+            this.lblMaKH.Text = "Mã Khách Hàng";
+            // 
+            // frmKhachHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 261);
+            this.Controls.Add(this.panelKH);
+            this.Name = "frmKhachHang";
             this.Text = "frmKhachHang";
+            this.panelKH.ResumeLayout(false);
+            this.panelKH.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxKH)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelKH;
+        private System.Windows.Forms.Label lblTopic;
+        private System.Windows.Forms.PictureBox pictureBoxKH;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.ComboBox cmbGioiTinh;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Label lblGioiTinh;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Label lblGhichu;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.TextBox txtGhichu;
+        private System.Windows.Forms.ComboBox cmbLoaiKH;
+        private System.Windows.Forms.Label lblLoaiKH;
+        private System.Windows.Forms.TextBox txtSoDT;
+        private System.Windows.Forms.TextBox txtDC;
+        private System.Windows.Forms.TextBox txttenKH;
+        private System.Windows.Forms.TextBox txtMaKH;
+        private System.Windows.Forms.Label lblSoDT;
+        private System.Windows.Forms.Label lblDiaCHi;
+        private System.Windows.Forms.Label lblTenKH;
+        private System.Windows.Forms.Label lblMaKH;
     }
 }
