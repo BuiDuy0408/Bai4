564768cbe4fce48c52bc099829d63dca3a57a4b2
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-11-18 15:49:56 +0700

    thanh-add button ma cc, ten cc

--- QuanLyKhoHang/QuanLyKhoHang/frmNhaCungCap.Designer.cs
+++ QuanLyKhoHang/QuanLyKhoHang/frmNhaCungCap.Designer.cs
@@ -31,6 +31,10 @@
             this.label9 = new System.Windows.Forms.Label();
             this.cboTK = new System.Windows.Forms.ComboBox();
             this.txtTK = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMaNCC = new System.Windows.Forms.TextBox();
+            this.txtTenNCC = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label9
@@ -62,11 +66,48 @@
             this.txtTK.Size = new System.Drawing.Size(264, 20);
             this.txtTK.TabIndex = 77;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 78;
+            this.label1.Text = "Mã NCC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 79;
+            this.label2.Text = "Tên NCC";
+            // 
+            // txtMaNCC
+            // 
+            this.txtMaNCC.Enabled = false;
+            this.txtMaNCC.Location = new System.Drawing.Point(106, 81);
+            this.txtMaNCC.Name = "txtMaNCC";
+            this.txtMaNCC.Size = new System.Drawing.Size(222, 20);
+            this.txtMaNCC.TabIndex = 80;
+            // 
+            // txtTenNCC
+            // 
+            this.txtTenNCC.Location = new System.Drawing.Point(106, 115);
+            this.txtTenNCC.Name = "txtTenNCC";
+            this.txtTenNCC.Size = new System.Drawing.Size(222, 20);
+            this.txtTenNCC.TabIndex = 81;
+            // 
             // frmNhaCungCap
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(762, 279);
+            this.Controls.Add(this.txtTenNCC);
+            this.Controls.Add(this.txtMaNCC);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtTK);
             this.Controls.Add(this.cboTK);
             this.Controls.Add(this.label9);
@@ -82,5 +123,9 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.ComboBox cboTK;
         private System.Windows.Forms.TextBox txtTK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMaNCC;
+        private System.Windows.Forms.TextBox txtTenNCC;
     }
 }
