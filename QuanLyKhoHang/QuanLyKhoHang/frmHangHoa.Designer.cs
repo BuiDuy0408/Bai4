8fd14b89b0e2d0a078f7e04978f592c305347561
truong19961405 <54049414+truong19961405@users.noreply.github.com>
2019-11-18 22:57:17 +0700

    xtruong
    xtruong_hanghoa

---
+++ QuanLyKhoHang/QuanLyKhoHang/frmHangHoa.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace QuanLyKhoHang
+{
+    partial class frmHangHoa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label9 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txttk_TenHH = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 9);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(96, 17);
+            this.label9.TabIndex = 72;
+            this.label9.Text = "Tìm kiếm theo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 17);
+            this.label3.TabIndex = 88;
+            this.label3.Text = "Tên hàng hóa";
+            // 
+            // txttk_TenHH
+            // 
+            this.txttk_TenHH.Location = new System.Drawing.Point(253, 4);
+            this.txttk_TenHH.Margin = new System.Windows.Forms.Padding(4);
+            this.txttk_TenHH.Name = "txttk_TenHH";
+            this.txttk_TenHH.Size = new System.Drawing.Size(188, 22);
+            this.txttk_TenHH.TabIndex = 90;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(468, 4);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(91, 17);
+            this.label8.TabIndex = 91;
+            this.label8.Text = "Nhà sản xuất";
+            // 
+            // frmHangHoa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1114, 544);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txttk_TenHH);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label9);
+            this.Name = "frmHangHoa";
+            this.Text = "frmHangHoa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txttk_TenHH;
+        private System.Windows.Forms.Label label8;
+    }
+}
