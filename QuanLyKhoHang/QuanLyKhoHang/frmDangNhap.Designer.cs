c364db9f4dce77d4fcd64f96bf3043f78c6b495e
LuThaiHoc <50101866+LuThaiHoc@users.noreply.github.com>
2019-11-26 15:08:59 +0700

    Update frmDangNhap.Designer.cs
    background color

--- QuanLyKhoHang/QuanLyKhoHang/frmDangNhap.Designer.cs
+++ QuanLyKhoHang/QuanLyKhoHang/frmDangNhap.Designer.cs
@@ -140,6 +140,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(915, 371);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnThoat);
