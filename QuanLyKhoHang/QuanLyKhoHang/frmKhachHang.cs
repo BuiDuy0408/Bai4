6ed401ba5e761b5764a7433ef84b9a4bfc5a2ea3
BuiDuy0408 <54673782+BuiDuy0408@users.noreply.github.com>
2019-11-26 20:48:00 +0700

    Duy - khach hang

--- QuanLyKhoHang/QuanLyKhoHang/frmKhachHang.cs
+++ QuanLyKhoHang/QuanLyKhoHang/frmKhachHang.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLogic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,41 @@ namespace QuanLyKhoHang
         public frmKhachHang()
         {
             InitializeComponent();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+
+        }
+        ChiNhanh kh = new ChiNhanh();
+        int temp = 0;
+
+        private void KhachHang_Load(object sender, EventArgs e)
+        {
+            DKOThongTin(false);
+            //setNULL();
+            //HienThiDGV();
+        }
+
+        public void DKOThongTin(bool bl)
+        {
+            txttenKH.Enabled = txtSoDT.Enabled = txtMaKH.Enabled = txtDC.Enabled = cmbLoaiKH.Enabled = bl;
+            cmbGioiTinh.Enabled = txtGhichu.Enabled = bl;
         }
     }
 }
