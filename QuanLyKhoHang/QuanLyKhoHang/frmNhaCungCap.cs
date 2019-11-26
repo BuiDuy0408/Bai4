b2f71dd74da701694cf152c2f44a451236a873f0
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-11-26 13:35:23 +0700

    thanh - xu li form

--- QuanLyKhoHang/QuanLyKhoHang/frmNhaCungCap.cs
+++ QuanLyKhoHang/QuanLyKhoHang/frmNhaCungCap.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLogic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +17,58 @@ namespace QuanLyKhoHang
         {
             InitializeComponent();
         }
+        NhaCungCap ncc = new NhaCungCap();
+        private void frmNhaCungCap_Load(object sender, EventArgs e)
+        {
+            HienThi();
+            cboTK.SelectedIndex = 0;
+        }
+
+        public void HienThi()
+        {
+            dataGridView1.DataSource = ncc.ShowNCC("");
+
+            Init();
+        }
+        public void Init()
+        {
+            if (dataGridView1.RowCount > 1)
+            {
+                try
+                {
+                    txtMaNCC.Text = dataGridView1.Rows[0].Cells[0].Value.ToString();
+                    txtTenNCC.Text = dataGridView1.Rows[0].Cells[1].Value.ToString();
+                    txtDiaChi.Text = dataGridView1.Rows[0].Cells[2].Value.ToString();
+                    txtSDT.Text = dataGridView1.Rows[0].Cells[3].Value.ToString();
+                }
+                catch { }
+            }
+
+        }
+        public void SetNull()
+        {
+            txtTK.Text = "";
+            txtMaNCC.Text = "";
+            txtTenNCC.Text = "";
+            txtDiaChi.Text = "";
+            txtSDT.Text = "";
+        }
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            SetNull();
+            cboTK.Enabled = false;
+            txtTK.Enabled = false;
+            btnLuu.Enabled = true;
+            btnHuy.Enabled = true;
+            btnThem.Enabled = false;
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            ncc.UpdateNCC(txtMaNCC.Text, txtTenNCC.Text, txtDiaChi.Text, txtSDT.Text);
+            MessageBox.Show("Sửa dữ liệu thành công !!!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            HienThi();
+        }
+
     }
 }
